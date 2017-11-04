dffe345ceb8f550d92bc51332631df0f3c4828c9
tjh28 <tjh2822@g.rit.edu>
2017-11-04 15:31:18 -0500

    adding flight software

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 
 void setup() {
     // put your setup code here, to run once:
+    Wire.begin(0x77);
+    Serial.begin(9600);
+    Wire.onRequest(getAltitude);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
+    //Serial.println("whatever");
+    delay(100);
+}
+
+void getAltitude(){
+    float alt = 42.0;
+    Wire.send(alt);
 }
